f7e778676452fda253340db1ec4d47cb452f6555
Ryan Gulden <rgulden@live.com>
2021-05-30 11:09:17 -0500

    formatted

--- src/App.vue
+++ src/App.vue
@@ -12,7 +12,7 @@ $primary: #8c67ef;
 $primary-light: findLightColor($primary);
 $primary-dark: findDarkColor($primary);
 $primary-invert: findColorInvert($primary);
-$twitter: #4099FF;
+$twitter: #4099ff;
 $twitter-invert: findColorInvert($twitter);
 
 // Lists and maps
@@ -21,61 +21,61 @@ $custom-shades: null !default;
 
 // Setup $colors to use as bulma classes (e.g. 'is-twitter')
 $colors: mergeColorMaps(
-    (
-        "white": (
-            $white,
-            $black,
-        ),
-        "black": (
-            $black,
-            $white,
-        ),
-        "light": (
-            $light,
-            $light-invert,
-        ),
-        "dark": (
-            $dark,
-            $dark-invert,
-        ),
-        "primary": (
-            $primary,
-            $primary-invert,
-            $primary-light,
-            $primary-dark,
-        ),
-        "link": (
-            $link,
-            $link-invert,
-            $link-light,
-            $link-dark,
-        ),
-        "info": (
-            $info,
-            $info-invert,
-            $info-light,
-            $info-dark,
-        ),
-        "success": (
-            $success,
-            $success-invert,
-            $success-light,
-            $success-dark,
-        ),
-        "warning": (
-            $warning,
-            $warning-invert,
-            $warning-light,
-            $warning-dark,
-        ),
-        "danger": (
-            $danger,
-            $danger-invert,
-            $danger-light,
-            $danger-dark,
-        ),
+  (
+    "white": (
+      $white,
+      $black,
     ),
-    $custom-colors
+    "black": (
+      $black,
+      $white,
+    ),
+    "light": (
+      $light,
+      $light-invert,
+    ),
+    "dark": (
+      $dark,
+      $dark-invert,
+    ),
+    "primary": (
+      $primary,
+      $primary-invert,
+      $primary-light,
+      $primary-dark,
+    ),
+    "link": (
+      $link,
+      $link-invert,
+      $link-light,
+      $link-dark,
+    ),
+    "info": (
+      $info,
+      $info-invert,
+      $info-light,
+      $info-dark,
+    ),
+    "success": (
+      $success,
+      $success-invert,
+      $success-light,
+      $success-dark,
+    ),
+    "warning": (
+      $warning,
+      $warning-invert,
+      $warning-light,
+      $warning-dark,
+    ),
+    "danger": (
+      $danger,
+      $danger-invert,
+      $danger-light,
+      $danger-dark,
+    ),
+  ),
+  $custom-colors
 );
 
 // Links
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,19 +1,19 @@
 <template>
-  <b-navbar type="is-black" fixed-top=true>
+  <b-navbar type="is-black" fixed-top="true">
     <template #brand>
-      <b-navbar-item  href="/">
-          <img
-              src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-              alt="Lightweight UI components for Vue.js based on Bulma"
-          >
+      <b-navbar-item href="/">
+        <img
+          src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+          alt="Lightweight UI components for Vue.js based on Bulma"
+        />
       </b-navbar-item>
-            <b-navbar-item href="#" target="_blank">
+      <b-navbar-item href="#" target="_blank">
         <b-icon pack="fab" size="is-small" icon="github"></b-icon>
       </b-navbar-item>
-            <b-navbar-item href="#" target="_blank" >
+      <b-navbar-item href="#" target="_blank">
         <b-icon pack="fab" size="is-small" icon="instagram"></b-icon>
       </b-navbar-item>
-            <b-navbar-item href="#" target="_blank">
+      <b-navbar-item href="#" target="_blank">
         <b-icon pack="fab" size="is-small" icon="facebook"></b-icon>
       </b-navbar-item>
     </template>
